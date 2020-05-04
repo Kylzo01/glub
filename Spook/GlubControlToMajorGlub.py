48d368f91fb60e6e0d6d4b0b6499be29e6bc09d7
The Net Beaner <43371515+Skepsy-R@users.noreply.github.com>
2020-05-04 23:23:39 +0100

    Commenting
    Commented code to help readability

--- Spook/GlubControlToMajorGlub.py
+++ Spook/GlubControlToMajorGlub.py
@@ -1,14 +1,18 @@
+#import the necessary software
+
 from sending import sendMessage as send
 import RPi.GPIO as GPIO
 from gpiozero import MotionSensor
 
 
+#Create loop for selecting other Pi's
 try:
     
     while True:
-
+#Ask who the user would like to interact with
         who = input("Who do you want to contact?\nKheraass - K, Voltar - V, Bren - B, SpookFeesh - S\n")
         
+#Give options and requirements
         if who == "B":
             
             send("Fire")
@@ -27,15 +31,16 @@ try:
             
             print("glub")
 
+#Break command to exit the loop
         
         elif who == "Break":
             
             print(x)
             
-        else:
-            
+        else
+#Error output        
             print("Command not recognised. Try again")
-
 except:
     
+#Exit dialouge      
     print("Exit")
